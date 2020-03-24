c6ad90d4779c2350e72c6a8f971b1a4168ad51ca
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-03-24 17:45:30 +0100

    template for coding

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Company_v1_core
+{
+    class Employee
+    {
+        private String name;
+        private int salaryPerMonth;
+
+        public Employee(String name, int salaryPerMonth)
+        {
+            this.name = name;
+            this.salaryPerMonth = salaryPerMonth;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public virtual int GetSalaryPerMonth()
+        {
+            return salaryPerMonth;
+        }
+    }
+}
